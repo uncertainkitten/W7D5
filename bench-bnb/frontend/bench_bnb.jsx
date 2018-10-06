ddd51744d7ca289d7dec5a15e7a62b7a01544d66
Cassandra <uncertainkitten@gmail.com>
2018-10-05 17:13:58 -0700

    End of Day commit

--- bench-bnb/frontend/bench_bnb.jsx
+++ bench-bnb/frontend/bench_bnb.jsx
@@ -2,8 +2,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  postUser,
+  postSession,
+  deleteSession
+} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  window.postUser = postUser;
+  window.postSession = postSession;
+  window.deleteSession = deleteSession;
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
